7822c51415aac06e1bf2d86f835485310bd71024
周涛 <15535311+zhoutao113@user.noreply.gitee.com>
2026-01-15 21:02:44 +0800

    写了下一个字典序和三颜色排序两个中等题

---
+++ 1.15/nextPermutation.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int find_min(int tar,vector<int>& nums,int begin)
+    {
+        int ans = begin; 
+        int min = nums[begin];
+        for(auto i = begin;i<nums.size();i++)
+        {
+            if(nums[i]<min&&nums[i]>tar)
+            {
+                min = nums[i];
+                ans = i;
+            }
+        }
+        return ans;
+    }
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        if(n<=1)
+        return;
+        for(auto i{n-1};i>0;i--)
+        {
+
+            if(nums[i]>nums[i-1])
+            {
+                int tar = find_min(nums[i-1],nums,i);
+                swap(nums[tar],nums[i-1]);
+                sort(nums.begin()+i,nums.end());
+                return;
+            }
+        }
+        reverse(nums.begin(),nums.end());
+    }
+};
+class Solution {
+public:
+    int find_min(int tar,vector<int>& nums,int begin)
+    {
+        int ans = begin; 
+        int min = nums[begin];
+        for(auto i = begin;i<nums.size();i++)
+        {
+            if(nums[i]<min&&nums[i]>tar)
+            {
+                min = nums[i];
+                ans = i;
+            }
+        }
+        return ans;
+    }
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        if(n<=1)
+        return;
+        for(auto i{n-1};i>0;i--)
+        {
+
+            if(nums[i]>nums[i-1])
+            {
+                int tar = find_min(nums[i-1],nums,i);
+                swap(nums[tar],nums[i-1]);
+                sort(nums.begin()+i,nums.end());
+                return;
+            }
+        }
+        reverse(nums.begin(),nums.end());
+    }
+};
---
+++ 1.15/sortColors.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void exchange(int& a,int& b)
+    {
+        a ^= b;
+        b ^= a;
+        a ^= b;
+    }
+    void sortColors(vector<int>& nums) {
+        int red_count = 0;
+        int blue_count = 0;
+        int n = nums.size();
+        if(n<2)
+        {
+            return;
+        }
+        for(int i{0};i<n;i++)
+        {
+            if(nums[i]==0&&i!=red_count)
+            {
+                exchange(nums[i],nums[red_count]);
+                red_count++;
+                i--;
+            }
+            if(nums[i]==2&&i!=n-1)
+            {
+                exchange(nums[i],nums[n-1]);
+                blue_count++;
+                n--;
+                i--;
+            }
+        }
+    }
+};
